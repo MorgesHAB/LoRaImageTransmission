544eb1a5c651a34b9a326d7c50643ddbaba703f2
MorgesHAB <lionel.isoz@gmail.com>
2019-08-09 22:04:18 +0200

    raspistill

--- raspicam.sh
+++ raspicam.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-DATE=$("%Y-%m-%d_%H%M")
+NAME=ImgTx
 
-raspistill -vf -hf -o /home/pi/camera/$DATE.jpg
-
-# sudo ?
-raspiyuv -rgb -o $DATE.rgb
-echo -e "P3\n3280 2464\n255" > $DATE.ppm
-cat $DATE.rgb >> test.ppm
+raspistill -o $NAME.jpg
+convert -compress none $NAME.jpg $NAME.ppm
