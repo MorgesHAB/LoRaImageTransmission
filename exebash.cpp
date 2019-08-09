6286abb142154dbf967f5c66b0c77be9ebd58b74
MorgesHAB <lionel.isoz@gmail.com>
2019-08-09 22:10:06 +0200

    exe bash

---
+++ exebash.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  system("bash raspicam.sh");
+  cout << "terminé" << endl;
+  return 0;
+}
--- raspicam.sh
+++ raspicam.sh
@@ -3,4 +3,9 @@
 NAME=ImgTx
 
 raspistill -o $NAME.jpg
+# install before :
+# $   sudo apt install imagemagick
 convert -compress none $NAME.jpg $NAME.ppm
+
+# convert inverse :
+# $   convert img.ppm img.jpg
